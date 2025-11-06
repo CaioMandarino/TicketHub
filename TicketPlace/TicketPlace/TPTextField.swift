cd59b0ecca3f94dd16eae1f5c8ef6b2bb7fb01cc
Caio <caiomand04@gmail.com>
2025-11-06 10:31:51 -0300

    init project

---
+++ TicketPlace/TicketPlace/TPTextField.swift
@@ -0,0 +1,18 @@
+//
+//  TPTextField.swift
+//  TicketPlace
+//
+//  Created by Caio Mandarino on 06/11/25.
+//
+
+import SwiftUI
+
+struct TPTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TPTextField()
+}
---
+++ TicketPlace/TicketPlace/TicketPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketPlaceApp.swift
+//  TicketPlace
+//
+//  Created by Caio Mandarino on 06/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TicketPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
